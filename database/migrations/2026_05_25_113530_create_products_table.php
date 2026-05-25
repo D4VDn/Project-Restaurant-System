9141aa62cee38d363b489388299afd876ce9b7e4
Gustavo <ga6621165@gmail.com>
2026-05-25 12:23:42 -0300

    Deus é bom!

--- database/migrations/2026_05_25_113530_create_products_table.php
+++ database/migrations/2026_05_25_113530_create_products_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->text('description');
             $table->decimal('price', 8, 2);
             $table->string('category');
-            $table->timestamp();
+            $table->timestamps();
         });
     }
 
