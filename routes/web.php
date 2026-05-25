ac2f60839b6ec4fcf759cff494a1587456e9b4ce
Gustavo <ga6621165@gmail.com>
2026-05-25 11:31:30 -0300

    inicio

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Laravel funcionando!';
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
